922cfe88e244f1cf94405206ae245f9436b98da4
DellacquaD <d.dellacquaruiz@gmail.com>
2023-04-23 13:37:18 -0300

    se modifican estilos del about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,10 +10,7 @@ import profile from '../../assets/profileIMG.png';
 
 export const About = () => {
   const greetings = "Hello there!";
-  const aboutme = `I'm ${FirstName} ${LastName}, a multidisciplinary 
-                  designer & developer. I'm always down for something new and challenging!
-                  I'm here to help you create beautifully formatted websites. 
-                  My projects mostly includes web development.`;
+  const aboutme = `I'm ${FirstName} ${LastName}, a passionate full-stack developer with a love for design, innovation, and challenges. My mission is to help you give visibility to your product by creating an innovative, intuitive, and aesthetically pleasing website that can take your business to the next level.`;
 
   return (
     <section id="about" style={{ position: 'relative', display: 'grid', minHeight: '100vh', placeContent: 'center', margin: '40px 0px' }}>
@@ -31,14 +28,14 @@ export const About = () => {
           </div>
           <div className="_content_wrapper">
             <Typography component='h2' variant="h5">
-              <TextDecrypt text={`${greetings}`} />
+              <TextDecrypt text={`${greetings}`} sx={{ fontWeight: 600, letterSpacing: '1.1px', fontSize: '30px' }}/>
             </Typography>
             <p className="aboutme">
               {aboutme}
             </p>
             <a href="#contact" className="contact-btn">
               <i className="fas fa-terminal"></i>
-              <Typography component='span'> Send me a message.</Typography>
+              <Typography component='span' sx={{ fontWeight: 600, fontSize: '18px'  }}> Send me a message.</Typography>
             </a>
           </div>
         </div>
